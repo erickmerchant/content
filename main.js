68d307ab6568eae8e8d1b4de027f115349c999ba
Erick Merchant <erickmerchant@gmail.com>
2017-03-16 01:13:29 -0700

    More work

--- main.js
+++ main.js
@@ -6,9 +6,7 @@ const thenify = require('thenify')
 const glob = thenify(require('glob'))
 const fs = require('fs')
 const readFile = thenify(fs.readFile)
-const pathMatch = require('path-match')()
 const cson = require('cson-parser')
-const matcher = pathMatch(':categories+/:time.:slug.md')
 const Highlights = require('highlights')
 const Remarkable = require('remarkable')
 const highlighter = new Highlights()
@@ -28,38 +26,34 @@ const remarkable = new Remarkable({
   langPrefix: 'language-'
 })
 
-app.command('publish')
-.describe('Generate html from markdown content and a js template')
-.parameter('destination', 'html destination')
-.option('template', 'template module (default ./template.js)')
+app.command('update')
+.describe('Generate html from markdown content and js')
+.parameter('destination', 'where to save to')
+.option('require', 'a module to define your html (default ./html.js)')
 .option('content', 'content directory (default ./content/)')
 .option('watch', 'watch for changes')
 .alias('w', 'watch')
 .action(function (args) {
-  assert.equal(typeof args.get('destination'), 'string', 'destination expects a string')
+  assert.ok(args.has('destination'), 'destination is required')
 
-  ;['template', 'content'].forEach(function (directory) {
-    if (!args.has(directory)) {
-      args.set(directory, path.join(process.cwd(), directory))
-    }
-  })
+  const contentDir = path.join(process.cwd(), args.has('content') ? args.get('content') : 'content')
 
-  return glob(args.get('content') + '/**/*.md').then(function (files) {
+  const required = require(path.join(process.cwd(), args.has('require') ? args.get('require') : 'html.js'))
+
+  assert.equal(typeof required, 'function', 'the required module should be a function')
+
+  required({define, template})
+
+  return glob(contentDir + '/**/*.md').then(function (files) {
     return Promise.all(files.map((file) => {
-      let object = matcher(path.relative(args.get('content'), file)) || {}
-
-      if (object.time) {
-        object.time = new Date(Number(object.time))
-      }
+      let location = path.relative(contentDir, file).split('.').slice(0, -1).join('.')
 
       return readFile(file, 'utf-8').then(function (blocks) {
         blocks = blocks.split('---').map((block) => block.trim())
 
         if (blocks[0] === '') {
           blocks = blocks.slice(1)
-        }
 
-        if (blocks.length > 1) {
           Object.assign(object, cson.parse(blocks.shift()))
         }
 
@@ -70,19 +64,23 @@ app.command('publish')
     }))
   })
   .then(function (objects) {
-    const template = require(args.get('template'))
 
-    assert.equal(typeof template, 'function', 'template expects a function')
-
-    template(objects, save)
   })
 
-  function save (file, html) {
-    assert.equal(typeof file, 'string', 'path expects a string')
+  function define () {
 
-    assert.equal(typeof html, 'string', 'html expects a string')
+  }
 
-    fs.writeFile(path.join(process.cwd(), args.get('destination'), file), html, function (err) {
+  function template () {
+
+  }
+
+  function save (file, content) {
+    assert.equal(typeof file, 'string', 'file to save should be a string')
+
+    assert.equal(typeof content, 'string', 'content to save should be a string')
+
+    fs.writeFile(path.join(process.cwd(), args.get('destination'), file), content, function (err) {
       if (err) {
         throw err
       }
