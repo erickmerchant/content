c39adb721147b0e6fb5d42d5f9fe544381f0ba22
Erick Merchant <erickmerchant@gmail.com>
2017-12-30 01:05:09 -0800

    Work on a complete rewrite

--- cli.js
+++ cli.js
@@ -1,47 +1,140 @@
 #!/usr/bin/env node
 const command = require('sergeant')
-const error = require('sergeant/error')
-const assert = require('assert')
-const chalk = require('chalk')
-const chokidar = require('chokidar')
+// const error = require('sergeant/error')
+// const assert = require('assert')
+// const chalk = require('chalk')
+const watch = require('@erickmerchant/conditional-watch')
 const path = require('path')
 const thenify = require('thenify')
-const minify = require('html-minifier').minify
+// const minify = require('html-minifier').minify
 const glob = thenify(require('glob'))
 const fs = require('fs')
-const escape = require('escape-html')
+// const escape = require('escape-html')
+const slugify = require('slugg')
 const cson = require('cson-parser')
-const Highlights = require('highlights')
-const Remarkable = require('remarkable')
-const pathMatch = require('path-match')()
-const pathCompile = require('path-to-regexp').compile
+// const Highlights = require('highlights')
+// const Remarkable = require('remarkable')
+// const pathMatch = require('path-match')()
+const pathTo = require('path-to-regexp')
 const mkdirp = thenify(require('mkdirp'))
 const readFile = thenify(fs.readFile)
 const writeFile = thenify(fs.writeFile)
-const highlighter = new Highlights()
-const remarkable = new Remarkable({
-  highlight (code, lang) {
-    if (!lang) {
-      return escape(code)
-    }
-
-    code = highlighter.highlightSync({
-      fileContents: code.trim(),
-      scopeName: 'source.js'
-    })
-
-    return code
-  },
-  langPrefix: 'language-'
-})
-const configFile = path.join(process.cwd(), 'html.js')
-const contentDir = path.join(process.cwd(), 'content')
+const rename = thenify(fs.rename)
+// const highlighter = new Highlights()
+// const remarkable = new Remarkable({
+//   highlight (code, lang) {
+//     if (!lang) {
+//       return escape(code)
+//     }
+//
+//     code = highlighter.highlightSync({
+//       fileContents: code.trim(),
+//       scopeName: 'source.js'
+//     })
+//
+//     return code
+//   },
+//   langPrefix: 'language-'
+// })
 
 command('html', 'generate html from markdown and js', function ({parameter, option, command}) {
-  let action = refresh()({collection})
+  command('make', 'make a new markdown file', function ({parameter, option}) {
+    parameter('destination', {
+      description: 'the directory to save to',
+      required: true
+    })
 
-  parameter('destination', {
-    description: 'where to save to',
+    option('title', {
+      description: 'the title',
+      required: true
+    })
+
+    return function (args) {
+      const now = Date.now()
+
+      const slug = slugify(args.title)
+
+      const object = {title: args.title}
+
+      const file = path.join(args.destination, `${now}.${slug}.md`)
+
+      return mkdirp(path.dirname(file)).then(function () {
+        return writeFile(file, stringify(object))
+      })
+    }
+  })
+
+  command('move', 'move a markdown file', function ({parameter, option}) {
+    parameter('source', {
+      description: 'the file to move',
+      required: true
+    })
+
+    parameter('destination', {
+      description: 'the directory to move it to',
+      required: true
+    })
+
+    option('title', {
+      description: 'a new title'
+    })
+
+    option('update', {
+      description: 'update the date and time',
+      type: Boolean,
+      default: { value: false }
+    })
+
+    return function (args) {
+      let pathResult = pathTo(':time(\\d+).:slug.md').exec(path.basename(args.source))
+      let now
+      let slug
+
+      if (pathResult) {
+        now = pathResult[1]
+
+        slug = pathResult[2]
+      }
+
+      return readFile(args.source, 'utf-8').then(function (string) {
+        const object = parse(string)
+
+        if (args.update) {
+          now = Date.now()
+        }
+
+        if (args.title != null) {
+          slug = slugify(args.title)
+
+          object.title = args.title
+        }
+
+        if (!now) {
+          return Promise.reject(new Error('time not set'))
+        }
+
+        if (!slug) {
+          return Promise.reject(new Error('slug not set'))
+        }
+
+        const file = path.join(args.destination, `${now}.${slug}.md`)
+
+        return mkdirp(path.dirname(file)).then(function () {
+          return rename(args.source, file).then(function () {
+            return writeFile(file, stringify(object))
+          })
+        })
+      })
+    }
+  })
+
+  parameter('content', {
+    description: 'directory containing your markdown',
+    required: true
+  })
+
+  parameter('template', {
+    description: 'a js file that will generate html',
     required: true
   })
 
@@ -51,266 +144,110 @@ command('html', 'generate html from markdown and js', function ({parameter, opti
     default: { value: false }
   })
 
-  option('dev', {
-    description: 'run in dev mode',
-    type: Boolean,
-    default: { value: false }
-  })
-
   option('watch', {
     description: 'watch for changes',
     type: Boolean,
-    aliases: ['w']
+    aliases: ['w'],
+    default: { value: false }
   })
 
   return function (args) {
-    const dev = args.dev
+    watch(args.watch, args.content, function () {
+      return glob(path.join(args.content, '**/*.md')).then(function (files) {
+        return Promise.all(files.map(function (file) {
+          let pathResult = pathTo(':time(\\d+).:slug.md').exec(path.basename(file))
+          let object = {}
 
-    run()
+          if (pathResult) {
+            object.date = new Date(Number(pathResult[1]))
 
-    if (args.watch) {
-      chokidar.watch([contentDir + '/**/*.md', configFile], {ignoreInitial: true}).on('all', function () {
-        action = refresh()({collection})
+            object.slug = pathResult[2]
+          }
 
-        run()
+          object.categories = path.dirname(path.relative(args.content, file)).split('/')
+
+          return readFile(file, 'utf-8').then(function (string) {
+            return Object.assign(parse(string), object)
+          })
+        }))
       })
-    }
+      .then(function (content) {
+        const template = require(args.template)
 
-    function run () {
-      const guarded = new Map()
+        const guarded = new Map()
 
-      action({get, save, html, safe, link, dev})
+        const pages = []
 
-      function html (strings, ...vals) {
-        let result = ''
+        const result = template({content, route, html, safe, link})
 
-        strings.forEach(function (str, key) {
-          result += str
+        function route (definition) {
 
-          if (vals[key]) {
-            let val = vals[key]
-
-            if (guarded.has(val)) {
-              val = guarded.get(val)
-            } else {
-              val = escape(val)
-            }
-
-            if (Array.isArray(val)) {
-              val = val.join('')
-            }
-
-            result += val
-          }
-        })
-
-        return result.trim()
-      }
-
-      function safe (val) {
-        let symbol = Symbol('safe')
-
-        guarded.set(symbol, val)
-
-        return symbol
-      }
-
-      function save (file, content) {
-        if (Array.isArray(file)) {
-          file.forEach(function (file) {
-            save(file, content)
-          })
-        } else {
-          assert.equal(typeof file, 'string', 'file to save should be a string')
-
-          assert.equal(typeof content, 'string', 'content to save should be a string')
-
-          if (file.endsWith('/')) {
-            file = file + 'index.html'
-          } else {
-            file = file + '.html'
-          }
-
-          let fullFile = path.join(process.cwd(), args.destination, file)
-
-          if (!args.noMin) {
-            content = minify(content, {
-              collapseWhitespace: true,
-              removeComments: true,
-              collapseBooleanAttributes: true,
-              removeAttributeQuotes: true,
-              removeRedundantAttributes: true,
-              removeEmptyAttributes: true,
-              removeOptionalTags: true
-            })
-          }
-
-          mkdirp(path.parse(fullFile).dir).then(function () {
-            return writeFile(fullFile, content).then(function () {
-              console.log(chalk.green('\u2714') + ' saved ' + path.join(args.destination, file))
-            })
-          })
-          .catch(function (err) {
-            if (err) {
-              throw err
-            }
-          })
-        }
-      }
-
-      function get (routes, template) {
-        if (!Array.isArray(routes)) {
-          routes = [routes]
         }
 
-        glob(path.join(contentDir, '/**/*.md')).then(function (files) {
-          return Promise.all(files.map(function (file) {
-            let params = routes.reduce(function (result, route) {
-              if (!result) {
-                let match = pathMatch(route + '.md')(path.relative(contentDir, file))
+        function html (strings, ...vals) {
+          let result = []
 
-                if (match) {
-                  return match
-                }
+          strings.forEach(function (str, key) {
+            result.push(str)
+
+            if (vals[key]) {
+              let val = vals[key]
+
+              if (guarded.has(val)) {
+                val = guarded.get(val)
+              } else {
+                val = escape(val)
               }
 
-              return result
-            }, null)
+              if (Array.isArray(val)) {
+                val = val.join('')
+              }
 
-            if (params) {
-              return load(file, params)
+              result.push(val)
             }
-          }))
-        })
-        .then(function (content) {
-          return content.filter((content) => content != null)
-        })
-        .then(template)
-        .catch(function (err) {
-          if (err) {
-            throw err
-          }
-        })
-      }
+          })
 
-      function link (route, object) {
-        return pathCompile(route)(object)
-      }
-    }
-  }
+          return result
+        }
 
-  function collection (name, definition) {
-    definition({on, save, remove, read})
+        function safe (val) {
+          let symbol = Symbol('safe')
 
-    function on (state, description, definition) {
-      if (definition == null) {
-        definition = description
+          guarded.set(symbol, val)
 
-        description = state + ' a ' + name
-      }
+          return symbol
+        }
 
-      command(name + ':' + state, description, definition)
-    }
-
-    function save (route, object, action) {
-      let location = pathCompile(route)(object)
-      let file = location + '.md'
-
-      const locationObject = pathMatch(route)(location)
-
-      Object.keys(locationObject).forEach(function (key) {
-        delete object[key]
-      })
-
-      const fullFile = path.join(process.cwd(), 'content', file)
-
-      let content = object.content || ''
-
-      delete object.content
-
-      const body = '---\n' + cson.stringify(object, null, 2) + '\n---\n' + content + '\n'
-
-      mkdirp(path.parse(fullFile).dir).then(function () {
-        return writeFile(fullFile, body).then(function () {
-          console.log(chalk.green('\u2714') + ' saved ' + path.join('content', file))
-
-          if (action != null) {
-            action()
-          }
-        })
-      })
-      .catch(function (err) {
-        if (err) {
-          throw err
+        function link (route, object) {
+          return pathTo.compile(route)(object)
         }
       })
-    }
-
-    function remove (file, action) {
-      fs.unlink(file, function (err) {
-        if (err) throw err
-
-        if (action != null) {
-          action()
-        }
-      })
-    }
-
-    function read (file, route, action) {
-      let params = pathMatch(route + '.md')(path.relative(contentDir, file))
-
-      if (params) {
-        load(file, params, false).then(action)
-      } else {
-        action({})
-      }
-    }
+    })
   }
 })(process.argv.slice(2))
 
-function refresh () {
-  try {
-    delete require.cache[configFile]
+function parse (string) {
+  let object = {}
 
-    const required = require(configFile)
+  let blocks = string.split('---').map((block) => block.trim())
 
-    assert.equal(typeof required, 'function', 'the required module should be a function')
+  if (blocks[0] === '') {
+    blocks = blocks.slice(1)
 
-    return required
-  } catch (e) {
-    error(e)
-
-    process.exit(1)
+    object = cson.parse(blocks.shift())
   }
+
+  object.content = blocks.join('---')
+
+  return object
 }
 
-function load (file, params, parse = true) {
-  return readFile(file, 'utf-8').then(function (blocks) {
-    let object = {}
+function stringify (object) {
+  object = Object.assign({}, object)
 
-    blocks = blocks.split('---').map((block) => block.trim())
+  let content = object.content || ''
 
-    if (blocks[0] === '') {
-      blocks = blocks.slice(1)
+  delete object.content
 
-      Object.assign(object, cson.parse(blocks.shift()))
-    }
-
-    Object.assign(object, params)
-
-    let content = blocks.join('---')
-
-    if (parse) {
-      content = remarkable.render(content)
-    }
-
-    Object.assign(object, {content})
-
-    return object
-  })
-  .catch(function (err) {
-    if (err) {
-      throw err
-    }
-  })
+  return ['---', cson.stringify(object, null, 2), '---', content].join('\n')
 }
